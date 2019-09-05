73d96eccf0cfd90d0bd94110af0f13836278fce3
mfasly <mffasly@gmail.com>
2019-09-05 23:13:24 +0530

    Update nodejsdocker.groovy
    changed docker repo

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('mffasly/docker-jenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)

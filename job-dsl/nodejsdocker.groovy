1932e8e3b3d4a2568a6828f08f0a511897ff17ed
mfasly <mffasly@gmail.com>
2019-09-05 18:28:24 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('mffasly/docker-nodejs-demo')
+            repositoryName('wardviaene/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
